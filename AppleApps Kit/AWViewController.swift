808ee446a50a328f0937023fce2c8d20e44dde47
iMahdi <imahdi.dev@gmail.com>
2021-02-06 19:50:44 +0330

    one signal removed

---
+++ AppleApps Kit/AWViewController.swift
@@ -0,0 +1,117 @@
+//
+//  AWViewController.swift
+//  AppleApps Kit
+//
+//  Created by IntZero on 9/13/20.
+//
+
+import UIKit
+import WebKit
+
+class AWViewController: UIViewController, WKNavigationDelegate, UIWebViewDelegate, WKUIDelegate {
+    
+    @IBOutlet weak var mainWebView: WKWebView!
+    let loadingBackgroundView = UIView()
+    var loadingView: UIView = UIView()
+    var activityView: UIActivityIndicatorView?
+    var urlString : String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mainWebView.navigationDelegate = self
+        self.mainWebView.uiDelegate = self
+        loadWebPage()
+    }
+    
+    internal func loadWebPage(fromCache isCacheLoad: Bool = false) {
+        var url : URL!
+        if(self.urlString == nil){
+            url = URL(string: "https://app.appleapps.ir")
+        } else {
+            url = URL(string: self.urlString)
+        }
+        let request = URLRequest(url: url, cachePolicy: (isCacheLoad ? .returnCacheDataElseLoad: .reloadRevalidatingCacheData), timeoutInterval: 10)
+        //URLRequest(url: url)
+        DispatchQueue.main.async { [weak self] in
+            self?.mainWebView.load(request)
+        }
+    }
+    
+    func showActivityIndicator() {
+        
+        //        loadingBackgroundView.frame = self.view.bounds
+        //        loadingBackgroundView.backgroundColor = UIColor.black
+        //        loadingBackgroundView.alpha = 0.3
+        //        loadingBackgroundView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        //        self.view.addSubview(loadingBackgroundView)
+        activityView = UIActivityIndicatorView(style: .whiteLarge)
+        activityView?.center = self.view.center
+        
+        loadingView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        loadingView.alpha = 0.7
+        loadingView.center = self.view.center
+        loadingView.backgroundColor = UIColor.black
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        self.view.addSubview(loadingView)
+        self.view.addSubview(activityView!)
+        activityView?.startAnimating()
+    }
+    
+    func hideActivityIndicator(){
+        if (activityView != nil){
+            activityView?.stopAnimating()
+            //            loadingBackgroundView.alpha = 0
+            loadingView.alpha = 0
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if #available(iOS 13, *) {
+            return .darkContent
+        } else {
+            return .default
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Started to load")
+        if ((urlString == "http") || (urlString == "https")) {
+         showActivityIndicator()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Finished loading")
+        if ((urlString == "http") || (urlString == "https")) {
+            hideActivityIndicator()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping ((WKNavigationActionPolicy) -> Void)) {
+        if let url = navigationAction.request.url {
+            let urlString = url.absoluteString
+            let prefix = String(urlString.prefix(4))
+            if (!(prefix == "http")) {
+                print(urlString)
+                let app = UIApplication.shared
+                let newUrl = URL(string: urlString)!
+                if (app.canOpenURL(newUrl)) {
+                    app.open(newUrl, options: [:], completionHandler: nil)
+                }
+            }
+        }
+        decisionHandler(.allow)
+    }
+
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
+        guard let url = request.url, navigationType == .linkClicked else { return true }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        return false
+    }
+}
--- AppleApps Kit/AppDelegate.swift
+++ AppleApps Kit/AppDelegate.swift
@@ -9,28 +9,30 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+          return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let urlOpened = url.absoluteString
+        let modified = urlOpened.replace(target: "applappkit://", withString:"")
+        let mainStoryboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let initialViewController : AWViewController = mainStoryboard.instantiateViewController(withIdentifier: "AWViewController") as! AWViewController
+        initialViewController.urlString = modified
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
+extension String
+{
+    func replace(target: String, withString: String) -> String
+    {
+        return self.replacingOccurrences(of: target, with: withString, options: NSString.CompareOptions.literal, range: nil)
+    }
+}
